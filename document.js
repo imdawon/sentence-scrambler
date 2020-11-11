8141a85bc01e9be3d34e6cde94661f546ccbd8fd
Dawson Hensel <dawsonhensel@gmail.com>
2020-11-11 10:47:09 -0800

    fixed code formatting in all docs

--- document.js
+++ document.js
@@ -3,6 +3,6 @@ let inputValue = document.getElementById('sentence-input');
 let renderedText = document.getElementById('scrambled-text');
 
 // When the randomize sentence button is clicked, run main function
-document.getElementById('randomize-btn').addEventListener('click', function() {
+document.getElementById('randomize-btn').addEventListener('click', function () {
     renderedText.textContent = main(inputValue.value);
 });
--- main.js
+++ main.js
@@ -11,11 +11,11 @@ const generateWordFlip = choppedSentence => {
 }
 
 const shuffle = array => {
-    for(let i = array.length - 1; i > 0; i--){
+    for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * i)
         const temp = array[i]
         array[i] = array[j]
         array[j] = temp
-      }
-      return array;
+    }
+    return array;
 }
